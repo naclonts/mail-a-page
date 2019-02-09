7a03ab84626e773147823a4ec982bb2e3b457739
Nathan Clonts <nacclonts@gmail.com>
2019-02-09 12:33:07 -0700

    update documentation; add puppeteer

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Get a website by email
-This small Node app is designed to automatically retrieve a webpage, then email it to a distribution list at a specified time.
+This small Node app is designed to automatically retrieve a webpage, then email it to a distribution list at a specified time. Works with SPA's as well as server-rendered pages, including some support for authentication through a login page.
 
-Used for generating "reports" to email from data-filled websites.
+- Generate & email reports from dashboards
+- Record a site's generated content daily
+
 
 ## Workings
-Node spawns a child PhantomJS process, then emails out the resulting HTML with Nodemailer.
+The URL you provide is retrieved, parsed by Chromium, and allowed to execute until the network is idle for 500ms. The HTML contents are then emailed to addresses of your choosing.
+
+The section of the webpage that is emailed is determined by the `SITE_CLASS` variable in `.env`. The contents of the first HTML element with the given CSS class will be included.
+
 
 ## Install
-Install PhantomJS and add it to the system path.
+> Note: puppeteer will install a headless Chromium to your machine if it isn't already installed. Be aware!
+
+Run `npm install`.
 
 Create a `.env` file specifying the environment variables listed in `.env.example`.
 
-Email is set to go through Outlook by default. This could pretty easily be changed by modifying the `src/email.js` file.
+Email is set to go through Outlook by default. This could easily be changed by modifying the `src/email.js` file's `transporter` object.
--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env nodejs
 /**
- * Run phantom-script.js from a node process.
- *
- * Credit to Our Code World for their tutorial:
- *  https://ourcodeworld.com/articles/read/379/how-to-use-phantomjs-with-node-js
+ * 
  */
+
 // Libraries
 const moment = require('moment');
 const path = require('path');
-const spawn = require('child_process').spawn;
+const puppeteer = require('puppeteer');
+const { URL } = require('url');
+
 // Load .env file to process.env
 require('dotenv').config({path: __dirname+'/./../.env'});
 
@@ -16,14 +16,14 @@ require('dotenv').config({path: __dirname+'/./../.env'});
 const email = require('./email');
 
 
-// PhantomJS command-line args and options
-let args = [path.join(__dirname, 'phantom-script.js'),
-    /* args 1 and 2 */ process.env.LOGIN_URL, process.env.URL,
-    /* args 3 and 4 */ process.env.SITE_USERNAME, process.env.SITE_PASSWORD,
-    /* args 5 and 6 */ process.env.AUTH_COOKIE_NAME, process.env.SITE_CLASS
-];
-// PhantomJS executable path
-let phantomExecutable = process.env.PHANTOMJS_EXE || 'phantomjs';
+// // PhantomJS command-line args and options
+// let args = [path.join(__dirname, 'phantom-script.js'),
+//     /* args 1 and 2 */ process.env.LOGIN_URL, process.env.URL,
+//     /* args 3 and 4 */ process.env.SITE_USERNAME, process.env.SITE_PASSWORD,
+//     /* args 5 and 6 */ process.env.AUTH_COOKIE_NAME, process.env.SITE_CLASS
+// ];
+// // PhantomJS executable path
+// let phantomExecutable = process.env.PHANTOMJS_EXE || 'phantomjs';
 
 
 /**
@@ -35,41 +35,6 @@ function Uint8ArrayToString(uint8Arr) {
     return String.fromCharCode.apply(null, uint8Arr);
 }
 
-
-// Create PhantomJS process
-let child = spawn(phantomExecutable, args);
-let output = '';
-
-// Receive output
-child.stdout.on('data', (data) => {
-    let text = Uint8ArrayToString(data);
-    console.log(text);
-    output += text;
-});
-child.stderr.on('data', (err) => {
-    console.log(`[${moment()}] Error in PhantomJS process!`);
-    let text = Uint8ArrayToString(err);
-    console.log(text);
-});
-// When finished, ship it off!
-child.on('close', async (code) => {
-    console.log(`[${moment()}] Process closed with status code: ${code}`);
-    // Email results
-    let options = {
-        from: process.env.NODEMAILER_USER,
-        to: process.env.EMAIL_TO.split(';'),
-        subject: `${process.env.EMAIL_SUBJECT} - ${moment().format("M/D/Y")}`,
-        text: '',
-        html: processHtml(output)
-    };
-    let res = await email.send(options);
-    if (!res.accepted || res.accepted.length == 0) {
-        console.error(`[${moment()}] Email not accepted! Response: ${res}`);
-    }
-    if (res.rejected && res.rejected.length > 0) {
-        console.error(`[${moment()}] Email rejected. Response: ${res}`);
-    }
-});
 
 // Add any desired inline styles here (before being sent in email).
 function processHtml(html) {
@@ -83,3 +48,57 @@ function processHtml(html) {
 
     return res;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Create PhantomJS process
+// let child = spawn(phantomExecutable, args);
+// let output = '';
+
+// // Receive output
+// child.stdout.on('data', (data) => {
+//     let text = Uint8ArrayToString(data);
+//     console.log(text);
+//     output += text;
+// });
+// child.stderr.on('data', (err) => {
+//     console.log(`[${moment()}] Error in PhantomJS process!`);
+//     let text = Uint8ArrayToString(err);
+//     console.log(text);
+// });
+// // When finished, ship it off!
+// child.on('close', async (code) => {
+//     console.log(`[${moment()}] Process closed with status code: ${code}`);
+//     // Email results
+//     let options = {
+//         from: process.env.NODEMAILER_USER,
+//         to: process.env.EMAIL_TO.split(';'),
+//         subject: `${process.env.EMAIL_SUBJECT} - ${moment().format("M/D/Y")}`,
+//         text: '',
+//         html: processHtml(output)
+//     };
+//     let res = await email.send(options);
+//     if (!res.accepted || res.accepted.length == 0) {
+//         console.error(`[${moment()}] Email not accepted! Response: ${res}`);
+//     }
+//     if (res.rejected && res.rejected.length > 0) {
+//         console.error(`[${moment()}] Email rejected. Response: ${res}`);
+//     }
+// });

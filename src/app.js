a3913cea598f8a729bdd908bb0f4111edbc81a99
kokopelli314 <nacclonts@gmail.com>
2019-03-28 10:27:27 -0600

    waitUntil networkidle0 (0 connections instead of 2)

--- src/app.js
+++ src/app.js
@@ -66,7 +66,7 @@ async function start() {
     //////////////////////////////
     // Go to the target page
     await page.goto(SITE_URL, {
-        waitUntil: 'networkidle2',
+        waitUntil: 'networkidle0',
 	timeout: 0, // never timeout
     });
 
